a3beedcf3e7b2705d7ee1101ec6ae28ab2162b3d
Roman Kohutyak <romankohutyak@gmail.com>
2020-12-15 20:37:41 +0200

    feat: new task

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -45,8 +45,12 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  function func(num) {
+    return num ** exponent;
+  }
+
+  return func;
 }
 
 /**
